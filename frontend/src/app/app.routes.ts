c6bea7dec8e8a9f94becd1fbe0cffc99a7fa4372
Jona <andritiana57@gmail.com>
2025-03-18 09:31:23 +0300

    Front Login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginViewComponent } from './components/login-view/login-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'', 
+        component: LoginViewComponent,
+        title: "Log In"
+    }
+];
